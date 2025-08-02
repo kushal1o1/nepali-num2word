81b5e9f0060807eecfdbf94e60f5c164c105c9a2
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-08-02 15:33:38 +0545

    fix:align with defined params too for now fallback to en although np

--- cli/main.py
+++ cli/main.py
@@ -1,10 +1,10 @@
-### cli/main.py
 import argparse
 from nepali_num2word import convert_to_words
 
 def main():
     parser = argparse.ArgumentParser(description='Convert number to words in Nepali style (crore, lakh, thousand).')
     parser.add_argument('number', type=str, help='Enter number to convert (int or float)')
+    parser.add_argument('--lang', choices=['en', 'np'], default='en', help='Language output (en for English, np for Nepali)')
     args = parser.parse_args()
 
     try:
@@ -14,7 +14,7 @@ def main():
         else:
             number = int(args.number)
         
-        result = convert_to_words(number)
+        result = convert_to_words(number, lang=args.lang)
         print(result)
     except Exception as e:
         print(f"Error: {e}")
