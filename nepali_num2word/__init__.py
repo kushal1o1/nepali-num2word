d1da02fa3b9e2544c0bf36d891a63f08d540d0d7
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-08-02 19:24:28 +0545

    Release v0.1.1 - Ready for PyPI

--- nepali_num2word/__init__.py
+++ nepali_num2word/__init__.py
@@ -13,7 +13,7 @@ Main functions:
 
 from .core import convert_to_words, format_number, compact_number
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Kushal"
 __email__ = "work.kusal@gmail.com"
 
